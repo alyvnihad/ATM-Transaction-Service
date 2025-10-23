c33092ca81215423d36b0ed3a55337140c908564
alyvnihad <aliyevnihad011@gmail.com>
2025-10-23 12:07:07 +0400

    AccountPayload update.v1 commit - Transaction Service

--- src/main/java/org/example/transactionservice/payload/AccountPayload.java
+++ src/main/java/org/example/transactionservice/payload/AccountPayload.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class AccountPayload {
     private Long accountId;
     private Long cardNumber;
+    private String email;
     private String type;
     private Double amount;
     private Long atmId;
