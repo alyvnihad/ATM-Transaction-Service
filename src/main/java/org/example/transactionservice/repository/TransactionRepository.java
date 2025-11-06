83a789b6ddf05dd378c742869a4e0a6f5c69f79f
alyvnihad <aliyevnihad011@gmail.com>
2025-11-07 00:06:30 +0400

    TransactionRepository update.v1 commit - Transaction Service

--- src/main/java/org/example/transactionservice/repository/TransactionRepository.java
+++ src/main/java/org/example/transactionservice/repository/TransactionRepository.java
@@ -2,10 +2,16 @@ package org.example.transactionservice.repository;
 
 import org.example.transactionservice.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
     List<Transaction> findByAccountId(Long accountId);
+
+    @Query("select oi from Transaction oi where function('date', oi.createdAt)=:date ")
+    List<Transaction> dailyTransaction (@Param("date")LocalDate date);
 }
